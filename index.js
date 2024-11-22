5ccb8c598eaed9845911d647e03b5b5c4249eb6f
Damian Neal Hattingh <damiannhattingh@gmail.com>
2024-11-22 22:42:20 +0200

    Created blackjack game, function to minus chips each new game and reward chips on blackjack
    Created blackjack game, function to minus chips each new game and reward chips on blackjack

--- index.js
+++ index.js
@@ -1,11 +1,78 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
+let player = {
+    name: "Damian",
+    chips: 100
+}
 
-// 2. Create a variable, sum, and set it to the sum of the two cards
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
 
-let firstCard = 6
-let secondCard = 9
 
-let sum = firstCard + secondCard
+function updateChipsDisplay() { // Function to update the player's chips display
+    playerEl.textContent = player.name + ": R" + player.chips //Change from $ to R
+}
 
-console.log(sum)
+updateChipsDisplay()// Initial display of player's chips
+
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
+function startGame() {
+    if (player.chips >= 10) { // Check if player has enough chips to start a new game
+        isAlive = true
+        hasBlackJack = false 
+        let firstCard = getRandomCard()
+        let secondCard = getRandomCard()
+        cards = [firstCard, secondCard]
+        sum = firstCard + secondCard
+        player.chips -= 10 // Deduct 10 chips for starting a new game
+        updateChipsDisplay()//Update chip count
+        renderGame()
+    } else {
+        messageEl.textContent = "Not enough chips to start a new game!"
+    }
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+        player.chips += 50 // Add 50 chips for getting Blackjack
+        updateChipsDisplay()
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()        
+    }
+}
